4878a89c98fc42f2485769326098c94eb2cfac75
unknown <tb.poopp@gmail.com>
2023-12-14 15:23:54 +0700

    feat add exercise async await

--- ex-5.js
+++ ex-5.js
@@ -12,3 +12,13 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+async function asynchronousFunction() {
+  try {
+    const JohnProfile = await getJohnProfile();
+    console.log(JohnProfile);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+asynchronousFunction();
